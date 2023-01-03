58a3a6f998aa2f03c5c92c7fbc65ede85e66b892
Gil Tayar <gil@tayar.org>
2023-01-03 12:19:11 +0200

    add css modules support and build and browser-sync

--- 03-no-tooling/src/index.js
+++ 03-no-tooling/src/index.js
@@ -2,10 +2,13 @@ import {html} from 'htm/react';
 import React from 'react'
 import ReactDOM from "react-dom/client";
 import App from "./App/index.js";
+import css from './index.css.json' assert {type: 'json'}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(html`
   <${React.StrictMode}>
-    <${App} />
+    <div class=${css.foo}>
+      <${App} class=${css.foo} />
+    <//>
   <//>
 `);
